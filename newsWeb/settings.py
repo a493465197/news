9f2f29becc7c12f91566c33e9e7d34576b074d66
李哲宇 <lzhey@servyou.com.cn>
2022-05-25 19:01:49 +0800

    fix

--- newsWeb/settings.py
+++ newsWeb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--9%z1#qx*a4f=$)y0*&x)r2j^ryvbv9(m#@l++y(hg_7xo%5c#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3607h09y06.qicp.vip']
 
 
 # Application definition
